8174d8fb115e5243fe4a6ae0119089e73594e43f
Martyna <martyna183@gmail.com>
2018-10-06 15:24:22 +0300

    is pirmo lango perejimas i aes algoritmo langa

--- app/src/main/java/com/example/martyna/sifravimas/MainActivity.java
+++ app/src/main/java/com/example/martyna/sifravimas/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.martyna.sifravimas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    private Button aes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aes = (Button) findViewById(R.id.aes);
+        aes.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openPirmas();
+            }
+        });
+    }
+    public void openPirmas(){
+        Intent intent = new Intent(this, Pirmas.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/martyna/sifravimas/Pirmas.java
@@ -0,0 +1,13 @@
+package com.example.martyna.sifravimas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pirmas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sifravimas);
+    }
+}
